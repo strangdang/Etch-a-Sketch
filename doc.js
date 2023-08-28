f2166781ee4eeae78cba47c22fab9a027b4f158a
Luke <137178874+strangdang@users.noreply.github.com>
2023-08-28 16:53:24 +0200

    added reset button to app

--- doc.js
+++ doc.js
@@ -1,30 +1,54 @@
-const sizeOfGrid = 16;
+const sizeOfGrid = 100;
 
-const ResetBtn = document.querySelector('button');
+const resetBtn = document.querySelector('button');
 const container = document.querySelector('.container');
 const color = [, "#3C9EE7", "#E7993C",
             "#E73C99", "#3CE746", "#E7993C"];
  
 
 const makeGrid = (amtOfCells) => {
+    const holder = document.createElement('div')
+    holder.classList.add('holder')
+
     for (let i = 0; i < amtOfCells; i++) {
-        const rows = document.createElement('div');
+        const rows = document.createElement('div'); 
         rows.classList.add('grid-row')
 
         for (let j = 0; j < amtOfCells; j++) {
-            const totalSize = 960 / sizeOfGrid
+            const totalSize = 960 / amtOfCells
             const columns = document.createElement('div');
             columns.classList.add('grid-column')
             columns.style.width = `${totalSize}px`
             columns.style.height = `${totalSize}px`
+
+            columns.addEventListener('mouseover', () => {
+                columns.style.backgroundColor = 'black'
+            })
             rows.appendChild(columns)
         }
-        container.appendChild(rows)
+        holder.appendChild(rows)
      }
+     container.appendChild(holder)
 }
-makeGrid (sizeOfGrid)
 
-//document.getElementsByClassName('.container').addEventListener("mouseover", (event) => {
+resetBtn.addEventListener('click', () => {
+    let userSize = Number(prompt('How many large do you want the grid?'))
+
+    while (userSize < 0 || userSize > 100) {
+        userSize = Number(prompt('Pick a number between 0 and 100'))
+    }
+    const holder = document.querySelector('.holder')
+    if (!holder) {
+        makeGrid(userSize)
+    } else {
+        holder.remove()
+        makeGrid(userSize)
+    }
+})
+
+makeGrid(sizeOfGrid)
+
+
    //// document.getElementsByClassName('.container').style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
 //});
 
